55d0379569dc348a0fd3998fc607cb63605a33d3
Yeon-Seon <48937186+Yeon-Seon@users.noreply.github.com>
2020-06-04 03:03:05 +0900

    Update device.sh

--- src/device.sh
+++ src/device.sh
@@ -2,24 +2,54 @@
 
 # refer to show_wifi_client.sh of OpenWrt
 # show IP, MAC address and hostname info for all connected Wi-Fi devices
-# echo "# All connected wifi devices, with IP address"
-# echo "# hostname (if available), and MAC address"
-# echo "# IP address, MAC address, hostname"
 
+# Extract IP Address, HW Address of ARP Table
+arp_table=`cat /proc/net/arp | cut -f 1,2,12-15,21-23 -s -d" "`
+
+mac_list=""
 # list all wireless network interfaces (for MAC80211 driver)
-for interface in `iw dev | grep Interface | cut -f 2 -s -d" "`
-do
+for interface in `iw dev | grep Interface | cut -f 2 -s -d" "`; do
   # for each interface, get mac addresses of connected stations/clients
-  mac_list=`iw dev $interface station dump | grep Station | cut -f 2 -s -d" "`
+  mac=`iw dev $interface station dump | grep Station | cut -f 2 -s -d" "`
+  mac_list="$mac_list $mac"
+done
 
-  # for each mac address in that list
-  for mac in $mac_list
-  do
-    # if a DHCP Lease has been given out by dnsmasq, save it
-    ip=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep $mac | cut -f 2 -s -d" "`
-    host=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep $mac | cut -f 3 -s -d" "`
+# for each mac address in that list
+for mac in $mac_list; do
+  arp_ip=`echo "$arp_table" | grep $mac | awk '{print $1}'`  
+  arp_ip_cnt=`echo "$arp_ip" | wc -l`
+  
+  arp_invalid_cnt=`echo "$arp_table" | grep $mac | awk '{print $2}' | grep 0x0 | wc -l`
+  arp_valid_cnt=`expr $arp_ip_cnt - $arp_invalid_cnt`
 
-    # show the mac address
-    echo -e "$ip\t$mac\t$host"
+  for ip in $arp_ip; do
+    dhcp_host=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep $ip | awk '{print $3}'`
+    if [ -z "$dhcp_host" ]; then
+      continue
+    fi
+
+    if [ $arp_valid_cnt -lt 2 ]; then
+      # show wifi client of normal state
+      echo -e "$ip\t$mac\t$dhcp_host\t-"
+    else
+      # show wifi client of abnormal state
+      dhcp_mac=`cat /tmp/dhcp.leases | cut -f 2,3 -s -d" " | grep $ip | awk '{print $1}'`
+      if [ -z "$dhcp_mac" ]; then
+        continue
+      fi
+
+      if echo $mac_list | grep -q $dhcp_mac; then
+        if [ "$mac" == "$dhcp_mac" ]; then
+          # show wifi client of attacker
+          echo -e "$ip\t$mac\t$dhcp_host\tHOST"
+        else
+          # show wifi client of victim
+          echo -e "$ip\t$mac\t$dhcp_host\tVICTIM"
+        fi
+      else
+        # show wifi client of vm
+        echo -e "$ip\t$mac\t$dhcp_host\tVM"
+      fi
+    fi
   done
 done
